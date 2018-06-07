56f0f5835bcd3daf5394046ae6653f9d881683e0
Jesus Lizama <dev@hellojeb.us>
2018-06-07 15:02:38 -0700

    get all users route

--- app/routes/dev.php
+++ app/routes/dev.php
@@ -1,8 +1,10 @@
 <?php
 
+use Puppers\User;
+
 $app->get('/dev', function($req, $res, $args){
-	$message = ["message" => "This is a development route"];
-	return $res->withStatus(200)->withJson($message);
+	$users = User::all();
+	return $res->withStatus(200)->withJson($users);
 });
 
 ?>
